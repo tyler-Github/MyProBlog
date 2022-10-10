dd8033641556b5eb1c500b1f2f0e6069a75d6bb2
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2022-10-10 19:13:11 +0100

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://blog.yasifys.tk',
   siteRepo: 'https://github.com/tyler-Github/MyProBlog',
-  siteLogo: '/static/images/LogoNew.png',
+  siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
