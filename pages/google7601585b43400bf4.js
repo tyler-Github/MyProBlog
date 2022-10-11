086daf961b87010d4f34890519b55d93b76dc36f
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2022-10-11 18:20:09 +0100

    Create google7601585b43400bf4.js

---
+++ pages/google7601585b43400bf4.js
@@ -0,0 +1,11 @@
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+
+export default function FourZeroFour() {
+  return (
+    <>
+     google-site-verification: google7601585b43400bf4.html
+    </>
+  )
+}
