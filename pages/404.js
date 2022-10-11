bab209da90f7415756bf939c7e1c20594d1ea75f
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2022-10-11 18:26:51 +0100

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -6,6 +6,7 @@ export default function FourZeroFour() {
   return (
     <>
       <PageSEO title={`Page Not Found - ${siteMetadata.title}`} />
+      <meta name="google-site-verification" content="EO7yA2E_SjQDtA2tX0qZkN0jZovxrIXjrhOuwZSzn1c" />
       <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
