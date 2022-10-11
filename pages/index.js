81243e038764847bbcb6eb42a8fa253d128c3138
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2022-10-11 18:30:28 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,6 @@ export default function Home({ posts }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
-      <meta name="google-site-verification" content="EO7yA2E_SjQDtA2tX0qZkN0jZovxrIXjrhOuwZSzn1c" />
-
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
