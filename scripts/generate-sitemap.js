18f46b129dc4bfce0949ac9a1049f29e5bc25ef3
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2022-10-12 17:37:48 +0100

    Update generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -11,7 +11,7 @@ const siteMetadata = require('../data/siteMetadata')
     'pages/*.tsx',
     'data/blog/**/*.mdx',
     'data/blog/**/*.md',
-    'public/author/**/*.xml',
+    'pages/author/**/*.xml',
     'public/tags/**/*.xml',
     '!pages/_*.js',
     '!pages/_*.tsx',
