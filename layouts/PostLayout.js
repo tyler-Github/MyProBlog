0b56be240aa10e136de9015aa9d503fb3dbb6aa5
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2022-10-12 18:39:50 +0100

    Update PostLayout.js

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -92,7 +92,7 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                   {'Discuss on Twitter'}
                 </Link>
                 {` • `}
-                <Link href="https://blog.yasifys.tk/author/"{author.name}>{'View Author'}</Link>
+                <Link href={`/author/${author.name}`}{'View Author'}</Link>
               </div>
               <Comments frontMatter={frontMatter} />
             </div>
