a8d4ff119362c85ac2f3756978e751f054853e79
tyler-Github <51767087+tyler-Github@users.noreply.github.com>
2022-11-28 18:31:26 +0000

    Update LayoutWrapper.js

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -27,9 +27,7 @@ const LayoutWrapper = ({ children }) => {
                 )}
               </div>
             </Link>
-          </div>
-<script type="text/javascript" src="https://cookieconsent.popupsmart.com/src/js/popper.js"></script><script> window.start.init({Palette:"palette6",Mode:"banner bottom",Time:"15",})</script>
-  
+          </div>  
           <div className="flex items-center text-base leading-5">
             <div className="hidden sm:block">
               {headerNavLinks.map((link) => (
